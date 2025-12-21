5d2e2027bbcac23a85d5779547cc97c3d8345ab1
BrownEdgar <43911617+BrownEdgar@users.noreply.github.com>
2025-12-21 20:36:28 +0400

    first commit

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,13 @@
+import './Post.css'
+
+function Post() {
+  return (
+    <div className="post">
+      <h2>This is a Title Component</h2>
+      <p>It is rendered inside the App component.</p>
+      <button>WOW</button>
+    </div>
+  )
+}
+
+export default Post
