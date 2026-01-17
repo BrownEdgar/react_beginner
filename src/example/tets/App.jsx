7e5d2dc7b0956a7ac9e4f4ddc0a4ac3dc1708875
BrownEdgar <43911617+BrownEdgar@users.noreply.github.com>
2026-01-17 15:54:33 +0400

    css module

---
+++ src/example/tets/App.jsx
@@ -0,0 +1,12 @@
+import s from './App.module.css';
+console.log(s);
+
+function App() {
+  return (
+    <div className={s.App}>
+      <h1 className={s.title}>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './example/fetchData/App.jsx'
+import App from './example/tets/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <App />
