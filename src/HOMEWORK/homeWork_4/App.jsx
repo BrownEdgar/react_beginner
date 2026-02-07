4668b4656ef8f0d03fd4d433f364d3aa34cc467d
BrownEdgar <43911617+BrownEdgar@users.noreply.github.com>
2026-02-07 15:47:12 +0400

    fix homework

--- src/HOMEWORK/homeWork_4/App.jsx
+++ src/HOMEWORK/homeWork_4/App.jsx
@@ -1,26 +1,25 @@
 import React, { useReducer, useState } from 'react'
 import reducer, { initialState } from './reducer'
- import "./App.css"
+import "./App.css"
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const [count, setCount]= useState(0);
-const handleClick = () => {
+  const handleClick = () => {
     setCount(item => item + 1);
   };
 
-
-    const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
       <h1> {state.join(", ")}</h1>
-      
+
       <h2> Count:{count}</h2>
-      <button onClick = {handleClick}>Click</button>
-      <button onClick={() => dispatch({type: "add", payload:Math.floor(Math.random() * 100) })}>add</button>
-      <button onClick={() => dispatch({type: "sort"})}> sort</button>
-      <button onClick={() => dispatch({type: "min"})}> min</button>
-      <button onClick={() => dispatch({type:"square"})}>square</button>
+      <button onClick={handleClick}>Click</button>
+      <button onClick={() => dispatch({ type: "add", payload: Math.floor(Math.random() * 100) })}>add</button>
+      <button onClick={() => dispatch({ type: "sort" })}> sort</button>
+      <button onClick={() => dispatch({ type: "min" })}> min</button>
+      <button onClick={() => dispatch({ type: "square" })}>square</button>
     </div>
   )
 }
